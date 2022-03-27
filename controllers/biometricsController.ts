a68a0f07c873e6e526f75397a315bf2e74998b9a
shakiru shekoni <shakirushekoni@shakirus-MacBook-Pro.local>
2022-03-27 19:25:14 +0100

    api

--- controllers/biometricsController.ts
+++ controllers/biometricsController.ts
@@ -37,7 +37,7 @@ export const biometrics = async (req: Request, res: Response) => {
 export const initiateEnroll = (req: Request, res: Response) => {
    try {
     console.log(req.body)
-    res.status(200).json({ data: req.body})
+    res.status(200).json(req.body)
    } catch (error: any) {
        res.status(400).send(error.message)
    }
